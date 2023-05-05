a7940c715c3cfa93a0fd93bdc469961cacf62323
elvismmoreira <40778346+elvismmoreira@users.noreply.github.com>
2023-05-04 21:28:40 -0300

    CRIADO CONTROLLER E CRUD LESSONS

--- course/src/main/java/com/ead/course/controllers/ModuleController.java
+++ course/src/main/java/com/ead/course/controllers/ModuleController.java
@@ -43,7 +43,7 @@ public class ModuleController {
     }
 
     @DeleteMapping("/courses/{courseId}/modules/{moduleId}")
-    public ResponseEntity<Object> deleteCourse(@PathVariable(value = "courseId") UUID courseId,
+    public ResponseEntity<Object> deleteModule(@PathVariable(value = "courseId") UUID courseId,
                                                @PathVariable(value = "moduleId")UUID moduleId) {
         Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourse(courseId, moduleId);
         if (!moduleModelOptional.isPresent()) {
